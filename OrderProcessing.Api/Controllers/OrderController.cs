e65257df802bd1dc1b34cf0b371c7af674218b53
andreluizleite <andreluiz_leite@hotmail.com>
2023-08-17 12:12:42 +0100

    starting the project, basic folders, files and layears

---
+++ OrderProcessing.Api/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderProcessing.Api.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
